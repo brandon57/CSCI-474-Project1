0a3f20bd45891bbba0647481cd01b8f6fafe682f
Brandon <bmsitarz@gmail.com>
2023-10-28 23:32:52 -0500

    Got everything laid out
    Just need to figure out why my I'm getting different numbers when I do
    different number of processes

--- Project1.c
+++ Project1.c
@@ -3,25 +3,33 @@
 #include <string.h>
 #include <errno.h>
 #include <time.h>
+#include <sys/wait.h>
 
 int display(int text);
 int input_Check(int option);
+int num_lines(void *file);
+
+//Global variables
+long long answer = 0;
+
+
 
 int main()
 {
     clock_t start_time, end_time;
     double total_time;
-    int size = 3;
     FILE* file;
-    char temp[size];
-    int total_lines = 0 , lines = 0;
+    
     int num_proc = 0;
     int num_file = 0;
+    int lines_proc = 0;
+    int total_lines = 0;
 
     //This is the part of the code where the 
 
     num_proc = display(1);
     num_file = display(2);
+
 
     //Selects what file to open based on what user picked
     switch(num_file)
@@ -38,15 +46,10 @@ int main()
     }
 
     //Counts how many lines are in the file
-    start_time = clock();
-    while(fscanf(file, "%s", temp) == 1)
-    {
-        total_lines++;
-    }
-    end_time = clock();
-    total_time = (double) (end_time - start_time) / CLOCKS_PER_SEC;
-    lines = total_lines / num_proc;
-    printf("There are %d lines in the file you chose\nIt took %lf seconds\n", total_lines, total_time);
+    total_lines = num_lines(file);
+    lines_proc = total_lines / num_proc;
+    int file_result[total_lines];
+    printf("There are %d lines in the file you chose\n", total_lines);
 
     //Moves the pointer back to the beginning of the file
     rewind(file);
@@ -61,22 +64,62 @@ int main()
     }
 
     //This creates the the number of processes the user selected
+    long proc_result;
+    long long results[num_proc];
+    int temp = 0;
+    int start, end = 0;
+    start_time = clock();
     for(int i = 0; i < num_proc; i++)
     {
         if(fork() == 0)
         {
-            break;
+            //read(fds[i][0], &start, sizeof(int));
+            //read(fds[i][0], &end, sizeof(int));
+            fseek(file, (lines_proc * i), ((lines_proc * i) + lines_proc));
+            printf("%d\n", i);
+            while(fscanf(file, "%d", &temp) == 1)
+            {
+                 proc_result = proc_result + temp;
+            }        
+            write(fds[i][1], &proc_result, sizeof(long));
+            //printf("%s\n", temp);
+            _exit(0);
         }
     }
-
 
     //parent waits for children to be done
     for(int i = 0; i < num_proc; i ++)
     {
-        
+        //write(fds[i][1], (lines_proc * i), sizeof(int));
+        //write(fds[i][1], (lines_proc * i), sizeof(int));
+        read(fds[i][0], &results[i], sizeof(long long));
+    }
+    end_time = clock();
+    wait(NULL);
+    total_time = (double) (end_time - start_time) / CLOCKS_PER_SEC;
+    
+    printf("It took this long: %f\n", total_time);
+    //wait(NULL);
+    
+    for(int i = 0; i < num_proc; i++)
+    {
+        answer = answer + results[i];
     }
 
+    printf("The total sum is: %lld\n", answer);
     
+}
+
+//Counts the amount of lines in the file
+int num_lines(void *file)
+{
+    char temp[3];
+    int total_lines = 0;
+    while(fscanf(file, "%s", temp) == 1)
+    {
+        total_lines++;
+    }
+    return total_lines;
 }
 
 //Creates the pipes
